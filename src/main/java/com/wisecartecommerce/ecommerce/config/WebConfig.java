552c2070402616ef2bc28aaaa184df68e134b6eb
albert <salbertjurcales37@gmail.com>
2026-03-24 14:37:43 +0800

    .

--- src/main/java/com/wisecartecommerce/ecommerce/config/WebConfig.java
+++ src/main/java/com/wisecartecommerce/ecommerce/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.wisecartecommerce.ecommerce.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
         String uploadLocation = "file:" + uploadDir + "/";
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations(uploadLocation)
-                .setCachePeriod(3600)
+                .setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS).cachePublic())
                 .resourceChain(true);
 
         registry.addResourceHandler("/swagger-ui.html")
