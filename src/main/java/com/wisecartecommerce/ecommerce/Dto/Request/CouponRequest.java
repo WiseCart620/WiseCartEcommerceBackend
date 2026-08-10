9ea8e817171603801dba5bdbd77e1d252053efd8
albert <salbertjurcales37@gmail.com>
2026-08-10 09:40:42 +0800

    .

--- src/main/java/com/wisecartecommerce/ecommerce/Dto/Request/CouponRequest.java
+++ src/main/java/com/wisecartecommerce/ecommerce/Dto/Request/CouponRequest.java
@@ -1,13 +1,20 @@
 package com.wisecartecommerce.ecommerce.Dto.Request;
 
-import com.wisecartecommerce.ecommerce.entity.Coupon;
-import jakarta.validation.constraints.*;
-import lombok.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
+
+import com.wisecartecommerce.ecommerce.entity.Coupon;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -24,7 +31,7 @@ public class CouponRequest {
     private Coupon.CouponType type;
 
     @NotNull(message = "Discount value is required")
-    @DecimalMin(value = "0.0", inclusive = false)
+    @DecimalMin(value = "0.0", inclusive = true, message = "Discount value cannot be negative")
     private BigDecimal discountValue;
 
     private BigDecimal minimumPurchaseAmount;
@@ -32,6 +39,7 @@ public class CouponRequest {
     private Integer maxUsageCount;
     private Integer maxUsagePerUser;
     private Integer minimumProductQuantity;
+    @Builder.Default
     private Integer shippingDiscountPercent = 100;
 
     @NotNull(message = "Start date is required")
@@ -40,10 +48,22 @@ public class CouponRequest {
     @NotNull(message = "Expiration date is required")
     private LocalDateTime expirationDate;
 
+    @Builder.Default
     private Boolean active = true;
     private Set<Long> applicableProducts;
     private Set<Long> applicableCategories;
+    @Builder.Default
     private Boolean combinable = false;
+    @Builder.Default
     private Set<Long> combinableWith = new HashSet<>();
+    @Builder.Default
     private Boolean automatic = false;
+
+    @AssertTrue(message = "Discount value must be greater than 0")
+    public boolean isDiscountValueValid() {
+        if (type == Coupon.CouponType.FREE_SHIPPING) {
+            return true;
+        }
+        return discountValue != null && discountValue.compareTo(BigDecimal.ZERO) > 0;
+    }
 }
